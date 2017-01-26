da8260a8c13745bfc7a8dbcb12e981f64ec7015e
ViperZeroOne <webmaster@viper01.com>
2017-01-25 22:07:00 -0500

    Minor Crash Fix
    Fixed a slight crash when placing the Velorean Chest... Yes, I know the
    chest is invisible and the textures don't work.  This is a work in
    progress.

--- src/main/java/com/viper01/vipercraft/tileentity/render/RenderVeloreanChest.java
+++ src/main/java/com/viper01/vipercraft/tileentity/render/RenderVeloreanChest.java
@@ -2,12 +2,16 @@ package com.viper01.vipercraft.tileentity.render;
 
 import com.viper01.vipercraft.tileentity.TileEntityVeloreanChest;
 
+import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
 
 public class RenderVeloreanChest extends TileEntitySpecialRenderer<TileEntityVeloreanChest> {
 
 	public void renderTileEntityAt(TileEntityVeloreanChest te, double x, double y, double z, float partialTicks, int destroyStage) {
 		super.renderTileEntityAt(te, x, y, z, partialTicks, destroyStage);
+
+		GlStateManager.pushMatrix();
+		GlStateManager.popMatrix();
 
 	}
 }
